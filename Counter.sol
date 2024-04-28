c695b0e722d1497f2dca254908d5fea4898bdfeb
ash <ashwin.sathiya@gmail.com>
2024-04-28 18:17:07 +0530

    added inc, dec, setname

--- Counter.sol
+++ Counter.sol
@@ -4,8 +4,25 @@ pragma solidity ^0.8.18;
 
 contract Counter {
     uint public count = 0;
+    string public name = "";
 
+    constructor (uint _count, string memory _name) {
+        name = _name;
+        count = _count;
+    }
     function increament () public {
         count ++ ;
     }
+
+    function decrement () public {
+        require(count > 0, "the couter must be greater than 0");
+        count -- ;
+    }
+
+    function setName (string memory _name) public returns (string memory newName) {
+        name = _name ;
+        return  name ;
+    }
+
+    
 }
